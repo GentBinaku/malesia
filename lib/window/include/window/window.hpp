7acfb1a3a93bc1f6bcf97dd5b5c246f8d23870d9
tengabenga <binakugent@gmail.com>
2026-08-21 00:00:40 +0200

    introduce taskfile

--- lib/window/include/window/window.hpp
+++ lib/window/include/window/window.hpp
@@ -4,43 +4,45 @@
 
 #ifndef MALESIA_WINDOWS_HPP
 #define MALESIA_WINDOWS_HPP
+// clang-format off
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <cstdint>
 #include <functional>
 #include <memory>
-#include <vector>
+// clang-format on
 
-namespace malesia
-{
-namespace window
-{
-    class Window
-    {
-    public:
-        using KeyCallback = std::function<void(int key, int scancode, int action, int mods)>;
+namespace malesia {
+namespace window {
+class Window {
+public:
+  using KeyCallback =
+      std::function<void(int key, int scancode, int action, int mods)>;
 
-        Window(uint32_t width, uint32_t height);
-        ~Window();
+  Window(uint32_t width, uint32_t height);
+  ~Window();
 
-        Window(const Window&) = delete;
-        Window& operator=(const Window&) = delete;
+  Window(const Window &) = delete;
+  Window &operator=(const Window &) = delete;
 
-        auto shouldClose() const -> bool;
-        auto pollEvents() const -> void;
-        auto swapBuffers() const -> void;
-        auto handle() const -> GLFWwindow*;
-        auto addKeyCallback(KeyCallback callback) -> void;
-    private:
-        using WindowHandle = std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)>;
+  auto shouldClose() const -> bool;
+  auto pollEvents() const -> void;
+  auto swapBuffers() const -> void;
+  auto handle() const -> GLFWwindow *;
+  auto setKeyCallback(KeyCallback callback) -> void;
 
-        static auto handleKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods) -> void;
+private:
+  using WindowHandle =
+      std::unique_ptr<GLFWwindow, decltype(&glfwDestroyWindow)>;
 
-        WindowHandle _window;
-        uint32_t _width;
-        uint32_t _height;
-        std::vector<KeyCallback> _keyCallbacks;
-    };
-}
-}
-#endif //MALESIA_WINDOWS_HPP
+  static auto handleKeyEvent(GLFWwindow *window, int key, int scancode,
+                             int action, int mods) -> void;
+
+  WindowHandle _window;
+  uint32_t _width;
+  uint32_t _height;
+  KeyCallback _keyCallback;
+};
+} // namespace window
+} // namespace malesia
+#endif // MALESIA_WINDOWS_HPP
